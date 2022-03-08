d388bc7785e7305f2ff442f40dd6a122ae8b8de0
zack <rubinzack40@gmail.com>
2022-03-08 01:55:41 -0500

    debug

--- app/rpi_gpio.py
+++ app/rpi_gpio.py
@@ -5,12 +5,13 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False) 
 BUTTON_DOWN=GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin to be an input pin and set initial value to be pulled low (off)
 
+if BUTTON_DOWN: print("Button pressed 8)")
 
-GPIO.setup(6,GPIO.OUT) # relay 1
-GPIO.output(6,GPIO.HIGH)
-GPIO.setup(13,GPIO.OUT) # relay 2
-GPIO.output(13,GPIO.HIGH)
-GPIO.setup(19,GPIO.OUT) # relay 3
-GPIO.output(19,GPIO.HIGH)
-GPIO.setup(26,GPIO.OUT) # relay 4
-GPIO.output(26,GPIO.HIGH)
+# GPIO.setup(6,GPIO.OUT) # relay 1
+# GPIO.output(6,GPIO.HIGH)
+# GPIO.setup(13,GPIO.OUT) # relay 2
+# GPIO.output(13,GPIO.HIGH)
+# GPIO.setup(19,GPIO.OUT) # relay 3
+# GPIO.output(19,GPIO.HIGH)
+# GPIO.setup(26,GPIO.OUT) # relay 4
+# GPIO.output(26,GPIO.HIGH)
