86f10cd6657d84aa525a59e0afbc9dc6802a1f8d
Dreadful20 <dreadful221@gmail.com>
2022-03-08 03:20:11 -0600

    k

--- app/main.py
+++ app/main.py
@@ -79,7 +79,7 @@ def button_test():
 def button_pressed(k):
     if rpi_gpio.GPIO.input(40) == rpi_gpio.GPIO.LOW: #button released
         print(f"Button released {k} times!")
-        k+=1
+    k+=1
         
 
 
