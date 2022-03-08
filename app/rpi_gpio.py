eb331689860f182d9a6f1fdb1db42ea8f844356d
Dreadful20 <dreadful221@gmail.com>
2022-03-08 03:46:50 -0600

    2

--- app/rpi_gpio.py
+++ app/rpi_gpio.py
@@ -3,7 +3,8 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setwarnings(False) 
-BUTTON_DOWN=GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin to be an input pin and set initial value to be pulled low (off)
+BUTTON_DOWN=GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+BUTTON_UP=GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin to be an input pin and set initial value to be pulled low (off)
 
 if GPIO.input(40) == GPIO.HIGH:
         print("Button was pushed!")
