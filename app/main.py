813b603111751c5105970087d3ea37c2256d0df0
zack <rubinzack40@gmail.com>
2022-03-08 01:12:43 -0500

    debug

--- app/main.py
+++ app/main.py
@@ -48,7 +48,10 @@ def main_menu():
 
         swrite("v", 4, 8)
 
-        if p==n-2: break
+        if p==n-2: 
+            swrite("", 2, 3)
+            swrite(">", 3, 3)
+            break
 
         sleep(1.5)
         p+=1
