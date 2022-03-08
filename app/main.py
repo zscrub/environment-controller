e13a3d9f0e6f25fb56e690c19f365b5f2e7b89f2
zack <rubinzack40@gmail.com>
2022-03-08 01:28:40 -0500

    test with long str

--- app/main.py
+++ app/main.py
@@ -43,8 +43,10 @@ def main_menu():
     while True:
         swrite("^", line=1, pos=8)
         swrite(">", 2, 3)
-        swrite(menu_list[p], 2, 5)
-        swrite(menu_list[p+1], 3, 5)
+
+        swrite(f"{p}.{menu_list[p]1}" 2, 5)
+        swrite(f"{p}.{menu_list[p+1}"], 3, 5)
+        
         print(menu_list[p])
         print(menu_list[p+1])
 
