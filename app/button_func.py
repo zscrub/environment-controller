33ddee6713944221ce030db231b4bace9f77fcf3
Dreadful20 <dreadful221@gmail.com>
2022-03-08 17:48:08 -0600

    more buttons

--- app/button_func.py
+++ app/button_func.py
@@ -4,17 +4,30 @@ from operator import truediv
 import rpi_gpio
 
 button_up_pressed = False
+button_up_released = False
 button_down_pressed = False
+button_down_released = False
 button_enter_pressed = False
+button_enter_released = False
+
 
 def button_up():
-    if button_up_pressed == False and rpi_gpio.GPIO.input(40) == rpi_gpio.GPIO.HIGH: #button pressed
-        button_up_pressed = True
+    while button_up_pressed == False and rpi_gpio.GPIO.input(40) == rpi_gpio.GPIO.HIGH: #button pressed
+        #button_up_pressed = True
+        if rpi_gpio.GPIO.input(40) == rpi_gpio.GPIO.LOW:
+            button_up_released = False
+        break
 
 def button_down():
-    if button_down_pressed == False and rpi_gpio.GPIO.input(38) == rpi_gpio.GPIO.HIGH: #button pressed
-        button_down_pressed == True
+    while button_down_pressed == False and rpi_gpio.GPIO.input(38) == rpi_gpio.GPIO.HIGH: #button pressed
+        #button_down_pressed == True
+        if rpi_gpio.GPIO.input(38) == rpi_gpio.GPIO.LOW:
+            button_down_released = False
+        break
 
 def button_enter_pressed():
-    if button_down_pressed == False and rpi_gpio.GPIO.input(36) == rpi_gpio.GPIO.HIGH: #button pressed
-        button_enter_pressed == True
+    while button_down_pressed == False and rpi_gpio.GPIO.input(36) == rpi_gpio.GPIO.HIGH: #button pressed
+        #button_enter_pressed == True
+        if rpi_gpio.GPIO.input(36) == rpi_gpio.GPIO.LOW:
+            button_enter_released = False
+        break
