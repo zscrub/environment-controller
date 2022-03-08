ccea6fc82ee669dd9bb7fe6db6e757fa808af083
zack <rubinzack40@gmail.com>
2022-03-07 23:29:14 -0500

    indent error

--- app/main.py
+++ app/main.py
@@ -37,8 +37,10 @@ def main_menu():
     n = len(menu_options)
     p = 0
     swrite("^", line=2, pos=8)
-        swrite(menu_options[p], line=2, pos=5)
-        swrite(menu_options[p+1], line=2, pos=5)
+
+    swrite(menu_options[p], line=2, pos=5)
+    swrite(menu_options[p+1], line=2, pos=5)
+    
     swrite("v", line=3, pos=8)
 
 
