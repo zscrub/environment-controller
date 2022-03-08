52247987eed05721b075370483a048351a3e6ff6
Dreadful20 <dreadful221@gmail.com>
2022-03-08 17:50:17 -0600

    print stuff

--- app/button_func.py
+++ app/button_func.py
@@ -14,6 +14,7 @@ button_enter_released = False
 def button_up():
     while button_up_pressed == False and rpi_gpio.GPIO.input(40) == rpi_gpio.GPIO.HIGH: #button pressed
         #button_up_pressed = True
+        print("up button pressed")
         if rpi_gpio.GPIO.input(40) == rpi_gpio.GPIO.LOW:
             button_up_released = False
         break
@@ -21,6 +22,7 @@ def button_up():
 def button_down():
     while button_down_pressed == False and rpi_gpio.GPIO.input(38) == rpi_gpio.GPIO.HIGH: #button pressed
         #button_down_pressed == True
+        print("down button pressed")
         if rpi_gpio.GPIO.input(38) == rpi_gpio.GPIO.LOW:
             button_down_released = False
         break
@@ -28,6 +30,7 @@ def button_down():
 def button_enter_pressed():
     while button_down_pressed == False and rpi_gpio.GPIO.input(36) == rpi_gpio.GPIO.HIGH: #button pressed
         #button_enter_pressed == True
+        print("enter button pressed")
         if rpi_gpio.GPIO.input(36) == rpi_gpio.GPIO.LOW:
             button_enter_released = False
         break
